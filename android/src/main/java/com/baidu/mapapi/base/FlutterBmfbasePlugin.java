197c360a0743978015076c15ff509175b84e9920
xp <c182828239@gmail.com>
2025-03-17 16:25:13 +0700

    导包

--- android/src/main/java/com/baidu/mapapi/base/FlutterBmfbasePlugin.java
+++ android/src/main/java/com/baidu/mapapi/base/FlutterBmfbasePlugin.java
@@ -11,14 +11,19 @@ import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.VersionInfo;
 import com.baidu.mapapi.common.BaiduMapSDKException;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.baidu.mapapi.base.BmfMapApplication;
+
 public class FlutterBmfbasePlugin implements FlutterPlugin, MethodCallHandler {
-  private MethodChannel channel;
+    private MethodChannel channel;
 
-  private static final String METHOD_SET_API_KEY = "flutter_bmfbase/sdk/setApiKey";
-  private static final String METHOD_GET_NATIVE_SDK_VERSION = "flutter_bmfbase/sdk/getNativeBaseVersion";
-  private static final String METHOD_SET_PRIVACY_API_KEY = "flutter_bmfbase/sdk/setAgreePrivacy";
+    private static final String METHOD_SET_API_KEY = "flutter_bmfbase/sdk/setApiKey";
+    private static final String METHOD_GET_NATIVE_SDK_VERSION = "flutter_bmfbase/sdk/getNativeBaseVersion";
+    private static final String METHOD_SET_PRIVACY_API_KEY = "flutter_bmfbase/sdk/setAgreePrivacy";
 
-  private static final String METHOD_GET_ANDROID_VERSION = "flutter_bmfbase/sdk/getAndroidSdkVersion";
+    private static final String METHOD_GET_ANDROID_VERSION = "flutter_bmfbase/sdk/getAndroidSdkVersion";
 
     @Override
     public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
@@ -33,40 +38,40 @@ public class FlutterBmfbasePlugin implements FlutterPlugin, MethodCallHandler {
 
     @Override
     public void onMethodCall(MethodCall call, Result result) {
-      if (call.method.equals(METHOD_GET_NATIVE_SDK_VERSION)) {
-        Map<String, String> versionMap = new HashMap<>();
-        versionMap.put("version", VersionInfo.getApiVersion());
-        versionMap.put("platform", "Android");
-        result.success(versionMap);
-      } else if(call.method.equals(METHOD_SET_API_KEY)){
-        if(call.hasArgument("BMF_COORD_TYPE")){
-          int nCoordType = call.argument("BMF_COORD_TYPE");
+        if (call.method.equals(METHOD_GET_NATIVE_SDK_VERSION)) {
+            Map<String, String> versionMap = new HashMap<>();
+            versionMap.put("version", VersionInfo.getApiVersion());
+            versionMap.put("platform", "Android");
+            result.success(versionMap);
+        } else if (call.method.equals(METHOD_SET_API_KEY)) {
+            if (call.hasArgument("BMF_COORD_TYPE")) {
+                int nCoordType = call.argument("BMF_COORD_TYPE");
 
-          /**
-           * flutter端的CoordType比android多了一个WGS-84，
-           * 因此获取到的nCoordType要减1才能与android端匹配
-           */
-          nCoordType = nCoordType - 1;
+                /**
+                 * flutter端的CoordType比android多了一个WGS-84，
+                 * 因此获取到的nCoordType要减1才能与android端匹配
+                 */
+                nCoordType = nCoordType - 1;
 
-          if(nCoordType >= 0 && CoordType.values().length > nCoordType){
-            CoordType coordType = CoordType.values()[nCoordType];
-            SDKInitializer.setCoordType(coordType);
-          }
+                if (nCoordType >= 0 && CoordType.values().length > nCoordType) {
+                    CoordType coordType = CoordType.values()[nCoordType];
+                    SDKInitializer.setCoordType(coordType);
+                }
 
-        }
-      } else if (call.method.equals(METHOD_SET_PRIVACY_API_KEY)) {
-        if (call.hasArgument("isAgree")) {
-          boolean isAgree = call.argument("isAgree");
-          if (BmfMapApplication.mContext != null) {
-            try {
-              SDKInitializer.setAgreePrivacy(BmfMapApplication.mContext, isAgree);
-            } catch (BaiduMapSDKException e) {
-              e.getMessage();
             }
-          }
+        } else if (call.method.equals(METHOD_SET_PRIVACY_API_KEY)) {
+            if (call.hasArgument("isAgree")) {
+                boolean isAgree = call.argument("isAgree");
+                if (BmfMapApplication.mContext != null) {
+                    try {
+                        SDKInitializer.setAgreePrivacy(BmfMapApplication.mContext, isAgree);
+                    } catch (BaiduMapSDKException e) {
+                        e.getMessage();
+                    }
+                }
+            }
+        } else if (call.method.equals(METHOD_GET_ANDROID_VERSION)) {
+            result.success(android.os.Build.VERSION.SDK_INT);
         }
-      } else if (call.method.equals(METHOD_GET_ANDROID_VERSION)) {
-        result.success(android.os.Build.VERSION.SDK_INT);
-      }
     }
 }
